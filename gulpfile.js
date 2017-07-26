558bce6fad246a7b7b7cae14aae914d28d47e564
Malander <marimichele26@gmail.com>
2017-07-26 15:42:37 +0200

    Revision Completed!

--- gulpfile.js
+++ gulpfile.js
@@ -25,8 +25,8 @@ var del             = require('del');
 |
 */
 
-gulp.task('sass', ['pug'], () => {
-  return gulp.src('app/assets/css/*.scss')
+gulp.task('scss', ['pug'], () => {
+  return gulp.src('app/assets/scss/*.scss')
     .pipe($.sass().on('error', $.sass.logError))
     .pipe($.autoprefixer('last 5 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1', 'ios 6', 'android 4'))
     .pipe($.cleanCss({compatibility: 'ie8'}))
